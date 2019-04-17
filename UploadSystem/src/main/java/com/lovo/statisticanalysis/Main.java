ad17347572b489d3f2ff567d461d3ada7334b720
Barrett <Barrett@DESKTOP-Barrett>
2019-04-17 17:03:05 +0800

    页面

---
+++ UploadSystem/src/main/java/com/lovo/statisticanalysis/Main.java
@@ -0,0 +1,17 @@
+package com.lovo.statisticanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		//springbootÈë¿Ú
+		SpringApplication.run(Main.class, args);
+	}
+
+}
