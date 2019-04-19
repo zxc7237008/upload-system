aa0554cc9097e7312456f0de7a8bb2b05ded0c59
Barrett <Barrett@DESKTOP-Barrett>
2019-04-19 11:08:30 +0800

    stt

--- UploadSystem/src/main/java/com/lovo/statisticanalysis/Application.java
+++ UploadSystem/src/main/java/com/lovo/statisticanalysis/Application.java
@@ -2,15 +2,14 @@ package com.lovo.statisticanalysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class Application {
 	
 	public static void main(String[] args) {
 		
-		//springbootÈë¿Ú
+		//springbootï¿½ï¿½ï¿½
 		SpringApplication.run(Application.class, args);
 	}
 
